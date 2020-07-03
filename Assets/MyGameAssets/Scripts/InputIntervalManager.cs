42934ebab57f1515630138abe4be448ddfa4c41e
tricore-as07 <tricore.as07@gmail.com>
2020-07-03 16:17:00 +0900

    InputIntervalを設定するScriptableObjectを作成

---
+++ Assets/MyGameAssets/Scripts/InputIntervalManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 入力から次の入力を受け付けるまでの時間を管理する
+/// </summary>
+public class InputIntervalManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
---
+++ Assets/MyGameAssets/Scripts/InputIntervalSettingData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class InputIntervalSettingData : ScriptableObject
+{
+    public List<InputIntervalSetting> inputIntervalSettings = new List<InputIntervalSetting>();
+} 
+
+[System.Serializable]
+public class InputIntervalSetting
+{
+    [SerializeField]
+    int ComboNum;
+    [SerializeField]
+    float intervalTime;
+
+}
