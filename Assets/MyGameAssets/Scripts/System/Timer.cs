d0c22022d5afa3afd0fa113a70c0a6cf81dee2f5
tricore-as07 <tricore.as07@gmail.com>
2020-07-09 18:11:58 +0900

    Timerクラス作成（未実装）

---
+++ Assets/MyGameAssets/Scripts/System/Timer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 制限時間のタイマー
+/// </summary>
+public class Timer : MonoBehaviour
+{
+    [SerializeField]float limitTime = 0f;       //制限時間
+
+    /// <summary>
+    /// 毎フレーム行う処理
+    /// </summary>
+    void Update()
+    {
+        limitTime -= Time.deltaTime;
+        //制限時間がなくなったら
+        if(limitTime <= 0)
+        {
+
+        }
+    }
+}
