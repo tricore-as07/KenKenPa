d2268871d7b360096a929c69ef2f662cc0699854
tricore-as07 <tricore.as07@gmail.com>
2020-07-28 13:17:28 +0900

    タイトルシーンにAdMobとNendの広告を表示する

--- Assets/MyGameAssets/Scripts/AdMobScript.cs
+++ Assets/MyGameAssets/Scripts/AdMobScript.cs
@@ -1,12 +1,18 @@
 ﻿using GoogleMobileAds.Api;
 using UnityEngine;
 
+/// <summary>
+/// AdMObの広告を表示するためのスクリプト
+/// </summary>
 public class AdMobScript : MonoBehaviour
 {
 
     public static BannerView bannerView;
     private AdRequest request;
 
+    /// <summary>
+    /// 最初に行う処理
+    /// </summary>
     void Start()
     {
         string appId = "ca-app-pub-4943898464524496~8024849235";
@@ -14,6 +20,9 @@ public class AdMobScript : MonoBehaviour
         RequestBanner();
     }
 
+    /// <summary>
+    /// バナー広告を表示する
+    /// </summary>
     public void RequestBanner()
     {
         string adUnitId = "ca-app-pub-3940256099942544/2934735716";
