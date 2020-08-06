e0a9d9d709a73ab0032a8d94abd3711750370776
tricore-as07 <tricore.as07@gmail.com>
2020-08-06 09:47:41 +0900

    プレハブでバナー広告を追加できるように NendとAdMobを指定回数ごとに切り替えれるように

---
+++ Assets/MyGameAssets/Scripts/BottomBanner.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using VMUnityLib;
+
+/// <summary>
+/// 下のバナーを表示するクラス
+/// </summary>
+public class BottomBanner : MonoBehaviour
+{
+    AdMobManager adMobManager;
+    NendAdController nendAdController;
+    UserAdData userAdData;
+
+    /// <summary>
+    /// スクリプトのインスタンスがロードされたときに呼ばれる
+    /// </summary>
+    void Awake()
+    {
+        adMobManager = AdMobManager.Inst;
+        nendAdController = NendAdController.Inst;
+        userAdData = UserAdData.Inst;
+    }
+
+    /// <summary>
+    /// オブジェクトがアクティブになった時によばれる
+    /// </summary>
+    void OnEnable()
+    {
+        if (userAdData.useAdMobBanner)
+        {
+            adMobManager.ShowBanner(AdMobManager.BANNER.BOTTOM,true);
+        }
+        else
+        {
+            nendAdController.ShowBottomBanner(true);
+        }
+        userAdData.OnShowAdBanner();
+    }
+
+    /// <summary>
+    /// オブジェクトが非アクティブになった時によばれる
+    /// </summary>
+    void OnDisable()
+    {
+        adMobManager.ShowBanner(AdMobManager.BANNER.BOTTOM,false);
+        nendAdController.ShowBottomBanner(false);
+    }
+}
+
