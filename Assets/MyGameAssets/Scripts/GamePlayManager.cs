f9cab9bc7c3fa7430302aab76e5ce4113cb50333
tricore-as07 <tricore.as07@gmail.com>
2020-08-06 12:35:35 +0900

    GamePlayManagerを作成

---
+++ Assets/MyGameAssets/Scripts/GamePlayManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ゲームプレイシーンの管理をするクラス
+/// </summary>
+public class GamePlayManager : MonoBehaviour
+{
+    [SerializeField] List<GameObject> manageObject = new List<GameObject>();        //管理するオブジェクト
+
+    /// <summary>
+    /// ゲームをスタートする時に呼ばれる
+    /// </summary>
+    public void GameStart()
+    {
+        foreach (var obj in manageObject)
+        {
+            obj.SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// ゲームをストップさせる時に呼ばれる
+    /// </summary>
+    public void GameStop()
+    {
+        foreach (var obj in manageObject)
+        {
+            obj.SetActive(false);
+        }
+    }
+}
