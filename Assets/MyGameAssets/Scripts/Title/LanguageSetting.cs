4cc5688284299912e8342c7fce40907b117d2486
tricore-as07 <tricore.as07@gmail.com>
2020-08-19 16:39:34 +0900

    設定ボタンと設定画面の作成

---
+++ Assets/MyGameAssets/Scripts/Title/LanguageSetting.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using I2.Loc;
+using TMPro;
+
+/// <summary>
+/// 言語設定
+/// </summary>
+public class LanguageSetting : MonoBehaviour
+{
+    /// <summary>
+    /// 設定言語が変更された時に呼ばれる
+    /// </summary>
+    public void OnChangeLanguage(TMP_Dropdown change)
+    {
+        if(change.value == 0)
+        {
+            LocalizationManager.CurrentLanguage = "Japanese";
+        }
+        else if (change.value == 1)
+        {
+            LocalizationManager.CurrentLanguage = "English";
+        }
+    }
+}
