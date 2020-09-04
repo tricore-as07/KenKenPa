3f8b4332678564fbc00aad4498373eb2fc35180b
tricore-as07 <tricore.as07@gmail.com>
2020-09-04 11:21:11 +0900

    タイトル画像のローカライズに対応

---
+++ Assets/MyGameAssets/Scripts/Title/TitleImage.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using I2.Loc;
+
+/// <summary>
+/// タイトルの画像を決める
+/// </summary>
+public class TitleImage : MonoBehaviour
+{
+    [SerializeField] GameObject titleImageJP = default;
+    [SerializeField] GameObject titleImageEN = default;
+
+    /// <summary>
+    /// スクリプトのインスタンスがロードされた時に呼ばれる
+    /// </summary>
+    void Awake()
+    {
+        if (LocalizationManager.CurrentLanguage == "Japanese")
+        {
+            titleImageJP.SetActive(true);
+            titleImageEN.SetActive(false);
+        }
+        else if (LocalizationManager.CurrentLanguage == "English")
+        {
+            titleImageJP.SetActive(false);
+            titleImageEN.SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// 設定言語が変更された時に呼ばれる
+    /// </summary>
+    public void OnChangeLanguage()
+    {
+        if (LocalizationManager.CurrentLanguage == "Japanese")
+        {
+            titleImageJP.SetActive(true);
+            titleImageEN.SetActive(false);
+        }
+        else if (LocalizationManager.CurrentLanguage == "English")
+        {
+            titleImageJP.SetActive(false);
+            titleImageEN.SetActive(true);
+        }
+    }
+}
