82e93f0c05eae5605d4fa9af334c54ed991c129a
tricore-as07 <tricore.as07@gmail.com>
2020-09-17 15:57:55 +0900

    スコアによってマテリアルを変更するスクリプトの途中

--- Assets/MyGameAssets/Scripts/System/ScoreSetter.cs
+++ Assets/MyGameAssets/Scripts/System/ScoreSetter.cs
@@ -6,7 +6,8 @@ using TMPro;
 /// </summary>
 public class ScoreSetter : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI scoreText;            //スコアのテキスト
+    [SerializeField] TextMeshProUGUI scoreText;                         //スコアのテキスト
+    [SerializeField] ScoreMaterialUiSetting scoreMaterialUiSetting;
     /// <summary>
     /// オブジェクトがアクティブになった時によばれる
     /// </summary>
@@ -14,6 +15,19 @@ public class ScoreSetter : MonoBehaviour
     {
         scoreText.text = ProgressDistanceCounter.DistanceCounter.ToString() + "m!";
         GameServiceUtil.ReportScore((int)ProgressDistanceCounter.DistanceCounter, 0);
+    }
+
+    /// <summary>
+    /// スコアによってマテリアルを別々に設定する
+    /// </summary>
+    /// NOTE : orimoto ここで設定されているスコア以上で設定されているマテリアルを設定する
+    [System.Serializable]
+    public class ScoreMaterialUiSetting
+    {
+        [SerializeField] int scoreNum = default;                        //コンボ数
+        public int ScoreNum => scoreNum;                                //外部に公開するためのプロパティ
+        [SerializeField] Material mat = default;                        //設定するマテリアル
+        public Material Mat => mat;                                     //外部に公開するためのプロパティ
     }
 }
 
