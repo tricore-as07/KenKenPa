88ce62212f63656bc6bd5eabc428b33dde1b2fdf
tricore-as07 <tricore.as07@gmail.com>
2020-09-18 11:05:40 +0900

    動く背景の作成途中

---
+++ Assets/MyGameAssets/Scripts/UI/CreateBackGroundUi.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 動く背景UIを作成する
+/// </summary>
+public class CreateBackGroundUi : MonoBehaviour
+{
+    [SerializeField] GameObject createUi = default;         //作成するUI
+    [SerializeField] float moveTime = default;              //移動にかける時間
+    [SerializeField] float createInterval = default;        //生成する間隔
+
+}
