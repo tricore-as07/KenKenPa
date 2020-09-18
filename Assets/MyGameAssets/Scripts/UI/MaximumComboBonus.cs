d71aa297e808642bc1fddf323509bb2cddccb42b
tricore-as07 <tricore.as07@gmail.com>
2020-09-18 15:13:17 +0900

    最大コンボボーナスの表示の見た目を最大コンボ数で変わるように

---
+++ Assets/MyGameAssets/Scripts/UI/MaximumComboBonus.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+/// <summary>
+/// 最大コンボボーナスの処理
+/// </summary>
+public class MaximumComboBonus : MonoBehaviour
+{
+    [SerializeField] List<ComboMaterialUiSetting> comboMaterialSettings = default;          //コンボ数によってマテリアルを設定するリスト
+    [SerializeField ] TextMeshProUGUI text;                                                 //テキスト
+    [SerializeField ] TextMeshProUGUI timeText;                                             //追加する時間のテキスト
+
+    /// <summary>
+    /// オブジェクトがアクティブになった時に呼ばれる
+    /// </summary>
+    void OnEnable()
+    {
+        foreach (var comboMaterialSetting in comboMaterialSettings)
+        {
+            if (comboMaterialSetting.ComboNum <= ComboCounter.MaxComboCount)
+            {
+                text.fontMaterial = comboMaterialSetting.Mat;
+                timeText.fontMaterial = comboMaterialSetting.Mat;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
