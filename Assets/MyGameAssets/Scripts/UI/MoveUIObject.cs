6086fca55c81648d4aa3be293727c95b63dd120a
tricore-as07 <tricore.as07@gmail.com>
2020-09-24 16:53:30 +0900

    必要のないスクリプトの削除とリファクタリング

--- Assets/MyGameAssets/Scripts/UI/MoveUIObject.cs
+++ Assets/MyGameAssets/Scripts/UI/MoveUIObject.cs
@@ -1,13 +1,21 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// UIを動かす
+/// </summary>
 public class MoveUIObject : MonoBehaviour
 {
-    [SerializeField] float moveTime = default;
-    float moveSpeed = default;
+    [SerializeField] float moveTime = default;      //画面の右端から左端まで動くのにかかる時間
+    float moveSpeed = default;                      //動くスピード
+    float halfScreenWidth = default;                //画面の半分の大きさ
 
+    /// <summary>
+    /// 最初に行う処理
+    /// </summary>
     void Start()
     {
-        moveSpeed = Screen.width / moveTime; 
+        moveSpeed = Screen.width / moveTime;
+        halfScreenWidth = Screen.width / 2;
     }
 
     /// <summary>
@@ -15,10 +23,15 @@ public class MoveUIObject : MonoBehaviour
     /// </summary>
     void Update()
     {
+        //左に自分を動かす
         transform.position += Vector3.left * moveSpeed * Time.deltaTime;
-        if(transform.position.x < -Screen.width / 2)
+        
+        //画面左端から画面の半分以上左にあるなら
+        if(transform.position.x < -halfScreenWidth)
         {
-            transform.position = Vector3.right * Screen.width * 1.5f;
+            //画面右端から画面の半分右に動かす
+            transform.position = Vector3.right * (Screen.width + halfScreenWidth);
+            //Y座標を画面の中心に戻す
             transform.position += Vector3.up * Screen.height / 2;
         }
     }
