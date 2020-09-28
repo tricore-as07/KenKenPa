2fa12a3fa975b4b8338fda0ca258c26516ac514a
tricore-as07 <tricore.as07@gmail.com>
2020-09-28 12:18:37 +0900

    UntiyAdsを使用してのテスト動画広告の表示

--- Assets/MyGameAssets/Scripts/System/FakeLoadManager.cs
+++ Assets/MyGameAssets/Scripts/System/FakeLoadManager.cs
@@ -7,7 +7,6 @@ public class FakeLoadManager : MonoBehaviour
 {
     [SerializeField] GameObject nativeAdObject = default;               //ネイティブ広告のオブジェクト
     [SerializeField] GameObject movieAdObject = default;                //動画広告のオブジェクト
-    [SerializeField] MovieAd movieAd = default;                         //動画広告に関するクラス
     [SerializeField] int movieAdPlayNum;                                //動画広告を表示するプレイ回数
     const string key = "PlayNum";                                       //ゲームのプレイ回数を保存するためのキー
     int playNum;                                                        //プレイ回数
@@ -20,15 +19,15 @@ public class FakeLoadManager : MonoBehaviour
         //プレイ回数を確認する
         playNum = PlayerPrefs.GetInt(key, 0);
         //動画広告を表示するプレイ回数だったら
-        if(playNum % movieAdPlayNum == 0)
+        if (playNum % movieAdPlayNum == 0)
         {
+            //動画広告の表示処理
             nativeAdObject.SetActive(false);
             movieAdObject.SetActive(true);
-            movieAd.Load();
-            movieAd.Show();
         }
         else
         {
+            //ネイティブ広告の表示処理
             nativeAdObject.SetActive(true);
             movieAdObject.SetActive(false);
         }
---
+++ Assets/MyGameAssets/Scripts/System/UnityAdsInitizer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.Advertisements;
+using UnityEngine;
+
+/// <summary>
+/// UnityAdsの初期化処理をする
+/// </summary>
+public class UnityAdsInitizer : MonoBehaviour
+{
+    [SerializeField] string gameId = default;         //UnityAdsのGameID
+    [SerializeField] bool testMode = default;         //テストモードで実行するかどうか
+
+    /// <summary>
+    /// スクリプトのインスタンスがロードされたときに呼ばれる
+    /// </summary>
+    void Awake()
+    {
+        Advertisement.Initialize(gameId, testMode);
+    }
+}
