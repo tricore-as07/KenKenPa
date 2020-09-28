33eafbf763eccde83a431570b88aa6ed78e7ac4c
tricore-as07 <tricore.as07@gmail.com>
2020-09-28 15:29:45 +0900

    ケンケンパっぽい挙動と自動生成

--- Assets/MyGameAssets/Scripts/UI/MoveCircleUI.cs
+++ Assets/MyGameAssets/Scripts/UI/MoveCircleUI.cs
@@ -5,17 +5,23 @@
 /// </summary>
 public class MoveCircleUI : MonoBehaviour
 {
-    [SerializeField] float moveTime = default;      //画面の下から上まで動くのにかかる時間
-    float moveSpeed = default;                      //動くスピード
-    float halfScreenHeight = default;               //画面の半分の大きさ
+    [SerializeField] float oneTripTime = default;                   //1回の移動時間
+    [SerializeField] int oneTripDistanceNum = default;              //1回に移動する画面分割数
+    [SerializeField] float InterpolationNum = default;              //補間に使う値
+    float nextMoveTime;                                             //次に動くまでの時間
+    float halfScreenHeight = default;                               //画面の半分の大きさ
+    float oneTripDistance;                                          //1回に移動する距離
+    Vector3 targetPos;
 
     /// <summary>
     /// 最初に行う処理
     /// </summary>
     void Start()
     {
-        moveSpeed = Screen.height / moveTime;
         halfScreenHeight = Screen.height / 2;
+        oneTripDistance = Screen.height / oneTripDistanceNum;
+        nextMoveTime = 0;
+        targetPos = transform.position + new Vector3(0f, -oneTripDistance, 0f);
     }
 
     /// <summary>
@@ -23,14 +29,22 @@ public class MoveCircleUI : MonoBehaviour
     /// </summary>
     void Update()
     {
-        //左に自分を動かす
-        transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+        //下に自分を動かす
+        nextMoveTime += Time.deltaTime;
+        transform.position = Vector3.Lerp(transform.position, targetPos, InterpolationNum);
+        if (nextMoveTime >= oneTripTime)
+        {
+            targetPos = transform.position + new Vector3(0f, -oneTripDistance, 0f);
+            nextMoveTime = 0;
+        }
 
-        //画面から画面の半分以上左にあるなら
-        if (transform.position.y > Screen.height + halfScreenHeight)
+
+        //画面から画面の半分以上下にあるなら
+        if (transform.position.y < -halfScreenHeight)
         {
             //画面右端から画面の半分右に動かす
-            transform.position += Vector3.down * (Screen.height * 2);
+            transform.position += Vector3.up * (Screen.height * 2);
+            targetPos += Vector3.up * (Screen.height * 2);
         }
     }
 }
